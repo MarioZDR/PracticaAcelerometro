ab2303bd45716eecf505d7ba51c6b7d6ce24607f
MarioZDR <125427714+MarioZDR@users.noreply.github.com>
2024-03-13 12:17:31 -0700

    Versión actualizada
    Versión actualizada con SharedPreferences y cambios en la forma en que se muestra

--- app/src/main/java/com/example/practicaacelerometro/MainActivity.kt
+++ app/src/main/java/com/example/practicaacelerometro/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.practicaacelerometro
-
 import android.content.Context
+import android.content.SharedPreferences
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -8,83 +8,80 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.IOException
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var gestorSensores: SensorManager
     private var acelerometro: Sensor? = null
-    private lateinit var vistaVelocidadActual: TextView
-    private lateinit var vistaVelocidadMaxima: TextView
-    private var velocidadMaxima = 0f
+
+    private lateinit var vistaAceleracionActual: TextView
+    private lateinit var vistaAceleracionMaxima: TextView
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private var aceleracionMaxima = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        vistaVelocidadActual = findViewById(R.id.velocidadActual)
-        vistaVelocidadMaxima = findViewById(R.id.velocidadMaxima)
+        vistaAceleracionActual = findViewById(R.id.aceleracionActual)
+        vistaAceleracionMaxima = findViewById(R.id.aceleracionMaxima)
+
+        sharedPreferences = getSharedPreferences("prefAcelerometro", Context.MODE_PRIVATE)
 
         gestorSensores = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         acelerometro = gestorSensores.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
-        velocidadMaxima = leerVelocidadMaxima()
-        vistaVelocidadMaxima.text = velocidadMaxima.toString()
+        aceleracionMaxima = leerAceleracionMaxima()
+        actualizarAceleracionMaxima(aceleracionMaxima)
     }
 
     override fun onResume() {
         super.onResume()
-        acelerometro?.let { gestorSensores.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL) }
+        gestorSensores.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
         gestorSensores.unregisterListener(this)
-        escribirVelocidadMaxima(velocidadMaxima)
     }
 
     override fun onSensorChanged(evento: SensorEvent) {
         if (evento.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-            val velocidad = sqrt(evento.values[0] * evento.values[0] +
+            val aceleracion = sqrt(evento.values[0] * evento.values[0] +
                     evento.values[1] * evento.values[1] +
                     evento.values[2] * evento.values[2])
 
-            vistaVelocidadActual.text = "Velocidad Actual: $velocidad"
+            actualizarAceleracionActual(aceleracion)
 
-            if (velocidad > velocidadMaxima) {
-                velocidadMaxima = velocidad
-                vistaVelocidadMaxima.text = "Velocidad Máxima: $velocidadMaxima"
+            if (aceleracion > aceleracionMaxima) {
+                aceleracionMaxima = aceleracion
+                actualizarAceleracionMaxima(aceleracionMaxima)
+                guardarAceleracionMaxima(aceleracionMaxima)
             }
         }
+    }
+
+    private fun actualizarAceleracionMaxima(aceleracionMaxima: Float){
+        vistaAceleracionMaxima.text = "Aceleración Máxima: ${String.format("%.1f", aceleracionMaxima)}"
+    }
+
+    private fun actualizarAceleracionActual(aceleracion: Float){
+        vistaAceleracionActual.text = "Aceleración Actual: ${String.format("%.1f", aceleracion)}"
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
     }
 
-    private fun leerVelocidadMaxima(): Float {
-        return try {
-            val fis = openFileInput("velocidad_maxima")
-            val bytes = ByteArray(fis.available())
-            fis.read(bytes)
-            fis.close()
-            String(bytes).toFloat()
-        } catch (e: IOException) {
-            0f
-        } catch (e: NumberFormatException) {
-            0f
-        }
+    private fun leerAceleracionMaxima(): Float {
+        return sharedPreferences.getFloat("aceleracionMaxima", 0f)
     }
 
-    private fun escribirVelocidadMaxima(velocidadMaxima: Float) {
-        try {
-            val fos = openFileOutput("velocidad_maxima", MODE_PRIVATE)
-            fos.write(velocidadMaxima.toString().toByteArray())
-            fos.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
+    private fun guardarAceleracionMaxima(aceleracionMaxima: Float) {
+        val editor = sharedPreferences.edit()
+        editor.putFloat("aceleracionMaxima", aceleracionMaxima)
+        editor.apply()
     }
 }
